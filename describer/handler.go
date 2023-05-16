1d87c6afe66c0e0ce946ea60f05e2a614e8a65bd
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-05-16 08:42:35 +0300

    feat: Adds docker

---
+++ describer/handler.go
@@ -0,0 +1,149 @@
+package describer
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/kaytu-io/kaytu-util/pkg/describe"
+	"github.com/kaytu-io/kaytu-util/pkg/vault"
+	golang2 "github.com/kaytu-io/kaytu-util/proto/src/golang"
+	"go.uber.org/zap"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/oauth"
+	"google.golang.org/grpc/metadata"
+	"os"
+	"time"
+)
+
+const (
+	DescribeResourceJobFailed    string = "FAILED"
+	DescribeResourceJobSucceeded string = "SUCCEEDED"
+)
+
+func getJWTAuthToken(workspaceId string) (string, error) {
+	privateKey, ok := os.LookupEnv("JWT_PRIVATE_KEY")
+	if !ok {
+		return "", fmt.Errorf("JWT_PRIVATE_KEY not set")
+	}
+
+	privateKeyBytes, err := base64.StdEncoding.DecodeString(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("JWT_PRIVATE_KEY not base64 encoded")
+	}
+
+	pk, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyBytes)
+	if err != nil {
+		return "", fmt.Errorf("JWT_PRIVATE_KEY not valid")
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"https://app.keibi.io/workspaceAccess": map[string]string{
+			workspaceId: "admin",
+		},
+		"https://app.keibi.io/email": "lambda-worker@kaytu.io",
+	}).SignedString(pk)
+	if err != nil {
+		return "", fmt.Errorf("JWT token generation failed %v", err)
+	}
+	return token, nil
+}
+
+func DescribeHandler(ctx context.Context, input describe.LambdaDescribeWorkerInput) error {
+	fmt.Printf("Input: %v", input)
+
+	var err error
+	logger := zap.NewNop()
+
+	if debug := os.Getenv("DEBUG"); debug == "true" {
+		logger, err = zap.NewProduction()
+		if err != nil {
+			return err
+		}
+	}
+
+	if input.WorkspaceName == "" {
+		return fmt.Errorf("workspace name is required")
+	}
+
+	kmsVault, err := vault.NewKMSVaultSourceConfig(ctx, "", "", input.KeyRegion)
+	if err != nil {
+		return fmt.Errorf("failed to initialize KMS vault: %w", err)
+	}
+
+	token, err := getJWTAuthToken(input.WorkspaceId)
+	if err != nil {
+		return fmt.Errorf("failed to get JWT token: %w", err)
+	}
+
+	resourceIds, err := Do(
+		ctx,
+		kmsVault,
+		logger,
+		input.DescribeJob,
+		input.KeyARN,
+		input.DescribeEndpoint,
+		token,
+		input.WorkspaceName,
+	)
+
+	errMsg := ""
+	status := DescribeResourceJobSucceeded
+	if err != nil {
+		errMsg = err.Error()
+		status = DescribeResourceJobFailed
+	}
+
+	for retry := 0; retry < 5; retry++ {
+		conn, err := grpc.Dial(
+			input.DescribeEndpoint,
+			grpc.WithTransportCredentials(credentials.NewTLS(nil)),
+			grpc.WithPerRPCCredentials(oauth.TokenSource{
+				TokenSource: oauth2.StaticTokenSource(&oauth2.Token{
+					AccessToken: token,
+				}),
+			}),
+		)
+		if err != nil {
+			logger.Error("[result delivery] connection failure:", zap.Error(err))
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		client := golang2.NewDescribeServiceClient(conn)
+
+		grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{
+			"workspace-name": input.WorkspaceName,
+		}))
+		_, err = client.DeliverResult(grpcCtx, &golang2.DeliverResultRequest{
+			JobId:       uint32(input.DescribeJob.JobID),
+			ParentJobId: uint32(input.DescribeJob.ParentJobID),
+			Status:      status,
+			Error:       errMsg,
+			DescribeJob: &golang2.DescribeJob{
+				JobId:         uint32(input.DescribeJob.JobID),
+				ScheduleJobId: uint32(input.DescribeJob.ScheduleJobID),
+				ParentJobId:   uint32(input.DescribeJob.ParentJobID),
+				ResourceType:  input.DescribeJob.ResourceType,
+				SourceId:      input.DescribeJob.SourceID,
+				AccountId:     input.DescribeJob.AccountID,
+				DescribedAt:   input.DescribeJob.DescribedAt,
+				SourceType:    string(input.DescribeJob.SourceType),
+				ConfigReg:     input.DescribeJob.CipherText,
+				TriggerType:   string(input.DescribeJob.TriggerType),
+				RetryCounter:  uint32(input.DescribeJob.RetryCounter),
+			},
+			DescribedResourceIds: resourceIds,
+		})
+		if err != nil {
+			logger.Error("[result delivery] rpc failed:", zap.Error(err))
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		break
+	}
+
+	return err
+}
---
+++ job/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/kaytu-io/kaytu-azure-describer/describer"
+	config2 "github.com/kaytu-io/kaytu-util/pkg/config"
+	"github.com/kaytu-io/kaytu-util/pkg/describe"
+	"github.com/kaytu-io/kaytu-util/pkg/queue"
+	"go.uber.org/zap"
+)
+
+func main() {
+	j := DescriberJob{}
+	panic(j.Run())
+}
+
+type DescriberConfig struct {
+	RabbitMQ  RabbitMQ
+	QueueName string
+}
+
+type RabbitMQ struct {
+	Service  string
+	Username string
+	Password string
+}
+
+type DescriberJob struct {
+	config      DescriberConfig
+	hopperQueue queue.Interface
+}
+
+func (h *DescriberJob) Run() error {
+	config2.ReadFromEnv(&h.config, nil)
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return err
+	}
+
+	logger.Info("Initializing the scheduler")
+	h.hopperQueue, err = initRabbitQueue(h.config.RabbitMQ, h.config.QueueName)
+	if err != nil {
+		return err
+	}
+
+	ch, err := h.hopperQueue.Consume()
+	if err != nil {
+		return err
+	}
+
+	for msg := range ch {
+		var job describe.LambdaDescribeWorkerInput
+		if err := json.Unmarshal(msg.Body, &job); err != nil {
+			logger.Error("failed to consume message from LambdaDescribeWorkerInput", zap.Error(err))
+			err = msg.Nack(false, false)
+			if err != nil {
+				logger.Error("failure while sending nack for message", zap.Error(err))
+			}
+			continue
+		}
+
+		err := describer.DescribeHandler(context.Background(), job)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func initRabbitQueue(cnf RabbitMQ, queueName string) (queue.Interface, error) {
+	qCfg := queue.Config{}
+	qCfg.Server.Username = cnf.Username
+	qCfg.Server.Password = cnf.Password
+	qCfg.Server.Host = cnf.Service
+	qCfg.Server.Port = 5672
+	qCfg.Queue.Name = queueName
+	qCfg.Queue.Durable = true
+	qCfg.Producer.ID = "describe-scheduler"
+	insightQueue, err := queue.New(qCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return insightQueue, nil
+}
