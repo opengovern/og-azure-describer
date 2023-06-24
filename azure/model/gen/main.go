1f2a3a6bc64086698e5b5dc18d069ac8c2763714
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-06-24 12:46:23 +0300

    feat: keibi-deploy -> kaytu-deploy

--- azure/model/gen/main.go
+++ azure/model/gen/main.go
@@ -45,7 +45,7 @@ type ResourceType struct {
 }
 
 func main() {
-	rt := "../../../keibi-deploy/keibi/inventory-data/azure-resource-types.json"
+	rt := "../../../kaytu-deploy/keibi/inventory-data/azure-resource-types.json"
 	b, err := os.ReadFile(rt)
 	if err != nil {
 		panic(err)
