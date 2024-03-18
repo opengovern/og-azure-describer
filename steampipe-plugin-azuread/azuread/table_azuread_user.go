478678526cd6e3fdb823c02d676bf8984bd2975c
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-03-18 12:34:00 +0400

    fix: Fixes tables

--- steampipe-plugin-azuread/azuread/table_azuread_user.go
+++ steampipe-plugin-azuread/azuread/table_azuread_user.go
@@ -51,43 +51,43 @@ func tableAzureAdUser(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 				Description: "The name displayed in the address book for the user. This is usually the combination of the user's first name, middle initial and last name.",
 
-				Transform: transform.FromField("Description.AdUsers.DisplayName")},
+				Transform: transform.FromField("Description.DisplayName")},
 			{
 				Name:        "id",
 				Type:        proto.ColumnType_STRING,
 				Description: "The unique identifier for the user. Should be treated as an opaque identifier.",
-				Transform:   transform.FromField("Description.AdUsers.DirectoryObject.ID"),
+				Transform:   transform.FromField("Description.ID"),
 			},
 			{
 				Name:        "user_principal_name",
 				Type:        proto.ColumnType_STRING,
 				Description: "Principal email of the active directory user.",
 
-				Transform: transform.FromField("Description.AdUsers.UserPrincipalName")},
+				Transform: transform.FromField("Description.UserPrincipalName")},
 			{
 				Name:        "account_enabled",
 				Type:        proto.ColumnType_BOOL,
 				Description: "True if the account is enabled; otherwise, false.",
 
-				Transform: transform.FromField("Description.AdUsers.AccountEnabled")},
+				Transform: transform.FromField("Description.AccountEnabled")},
 			{
 				Name:        "user_type",
 				Type:        proto.ColumnType_STRING,
 				Description: "A string value that can be used to classify user types in your directory.",
 
-				Transform: transform.FromField("Description.AdUsers.UserType")},
+				Transform: transform.FromField("Description.UserType")},
 			{
 				Name:        "given_name",
 				Type:        proto.ColumnType_STRING,
 				Description: "The given name (first name) of the user.",
 
-				Transform: transform.FromField("Description.AdUsers.GivenName")},
+				Transform: transform.FromField("Description.GivenName")},
 			{
 				Name:        "surname",
 				Type:        proto.ColumnType_STRING,
 				Description: "Family name or last name of the active directory user.",
 
-				Transform: transform.FromField("Description.AdUsers.Surname")},
+				Transform: transform.FromField("Description.Surname")},
 			{
 				Name:        "filter",
 				Type:        proto.ColumnType_STRING,
@@ -101,72 +101,72 @@ func tableAzureAdUser(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 				Description: "Used to associate an on-premises Active Directory user account with their Azure AD user object.",
 
-				Transform: transform.FromField("Description.AdUsers.OnPremisesImmutableId")},
+				Transform: transform.FromField("Description.OnPremisesImmutableId")},
 			{
 				Name:        "created_date_time",
 				Type:        proto.ColumnType_TIMESTAMP,
 				Description: "The time at which the user was created.",
 
-				Transform: transform.FromField("Description.AdUsers.CreatedDateTime")},
+				Transform: transform.FromField("Description.CreatedDateTime")},
 			{
 				Name:        "mail",
 				Type:        proto.ColumnType_STRING,
 				Description: "The SMTP address for the user, for example, jeff@contoso.onmicrosoft.com.",
 
-				Transform: transform.FromField("Description.AdUsers.Mail")},
+				Transform: transform.FromField("Description.Mail")},
 			{
 				Name:        "mail_nickname",
 				Type:        proto.ColumnType_STRING,
 				Description: "The mail alias for the user.",
 
-				Transform: transform.FromField("Description.AdUsers.MailNickname")},
+				Transform: transform.FromField("Description.MailNickname")},
 			{
 				Name:        "password_policies",
 				Type:        proto.ColumnType_STRING,
 				Description: "Specifies password policies for the user. This value is an enumeration with one possible value being DisableStrongPassword, which allows weaker passwords than the default policy to be specified. DisablePasswordExpiration can also be specified. The two may be specified together; for example: DisablePasswordExpiration, DisableStrongPassword.",
 
-				Transform: transform.FromField("Description.AdUsers.PasswordPolicies")},
+				Transform: transform.FromField("Description.PasswordPolicies")},
 			{
 				Name:        "refresh_tokens_valid_from_date_time",
 				Type:        proto.ColumnType_TIMESTAMP,
 				Description: "Any refresh tokens or sessions tokens (session cookies) issued before this time are invalid, and applications will get an error when using an invalid refresh or sessions token to acquire a delegated access token (to access APIs such as Microsoft Graph).",
 
-				Transform: transform.FromField("Description.AdUsers.RefreshTokensValidFromDateTime")},
+				Transform: transform.FromField("Description.RefreshTokensValidFromDateTime")},
 			{
 				Name:        "sign_in_sessions_valid_from_date_time",
 				Type:        proto.ColumnType_TIMESTAMP,
 				Description: "Any refresh tokens or sessions tokens (session cookies) issued before this time are invalid, and applications will get an error when using an invalid refresh or sessions token to acquire a delegated access token (to access APIs such as Microsoft Graph).",
 
-				Transform: transform.FromField("Description.AdUsers.SignInSessionsValidFromDateTime")},
+				Transform: transform.FromField("Description.SignInSessionsValidFromDateTime")},
 			{
 				Name:        "usage_location",
 				Type:        proto.ColumnType_STRING,
 				Description: "A two letter country code (ISO standard 3166), required for users that will be assigned licenses due to legal requirement to check for availability of services in countries.",
-				Transform:   transform.FromField("Description.AdUsers.UsageLocation")},
+				Transform:   transform.FromField("Description.UsageLocation")},
 
 			{
 				Name:        "member_of",
 				Type:        proto.ColumnType_JSON,
 				Description: "A list the groups and directory roles that the user is a direct member of.",
 
-				Transform: transform.FromField("Description.AdUsers.MemberOf")},
+				Transform: transform.FromField("Description.MemberOf")},
 			{
 				Name:        "additional_properties",
 				Type:        proto.ColumnType_JSON,
 				Description: "A list of unmatched properties from the message are deserialized this collection.",
-				Transform:   transform.FromField("Description.AdUsers.CreationType")},
+				Transform:   transform.FromField("Description.AdditionalProperties")},
 			{
 				Name:        "im_addresses",
 				Type:        proto.ColumnType_JSON,
 				Description: "The instant message voice over IP (VOIP) session initiation protocol (SIP) addresses for the user.",
 
-				Transform: transform.FromField("Description.AdUsers.ImAddresses")},
+				Transform: transform.FromField("Description.ImAddresses")},
 			{
 				Name:        "other_mails",
 				Type:        proto.ColumnType_JSON,
 				Description: "A list of additional email addresses for the user.",
 
-				Transform: transform.FromField("Description.AdUsers.OtherMails")},
+				Transform: transform.FromField("Description.OtherMails")},
 			{
 				Name:        "password_profile",
 				Type:        proto.ColumnType_JSON,
@@ -174,14 +174,14 @@ func tableAzureAdUser(_ context.Context) *plugin.Table {
 
 				Transform:
 				// Standard columns
-				transform.FromField("Description.AdUsers.PasswordProfile")},
+				transform.FromField("Description.PasswordProfile")},
 
 			{
 				Name:        "title",
 				Type:        proto.ColumnType_STRING,
 				Description: ColumnDescriptionTitle,
 
-				Transform: transform.FromField("Description.AdUsers.DisplayName")},
+				Transform: transform.FromField("Description.DisplayName")},
 			{
 				Name:        "tenant_id",
 				Type:        proto.ColumnType_STRING,
