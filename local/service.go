af1ac917d936776a2d3b1cc642c956771474ed90
artaasadi <arta-c@kaytu.io>
2024-10-24 13:27:07 +0200

    fix: kaytu to og

--- local/service.go
+++ local/service.go
@@ -20,10 +20,10 @@ import (
 )
 
 const (
-	StreamName           = "kaytu_azure_describer"
-	JobQueueTopic        = "kaytu_azure_describer_job_queue"
+	StreamName           = "og_azure_describer"
+	JobQueueTopic        = "og_azure_describer_job_queue"
 	ConsumerGroup        = "azure-describer"
-	JobQueueTopicManuals = "kaytu_azure_describer_manuals_job_queue"
+	JobQueueTopicManuals = "og_azure_describer_manuals_job_queue"
 	ConsumerGroupManuals = "azure-describer-manuals"
 )
 
